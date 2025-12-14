65fef655a9002b3a3432e39c2775675a25759859
giannistamoulakatos <79400029+giannistamoulakatos@users.noreply.github.com>
2025-12-14 13:32:34 +0200

    Update macchange.py

--- Scripts/macchange.py
+++ Scripts/macchange.py
@@ -1,6 +1,6 @@
 import os 
 
-down = os.system(f'sudo ip link set dev wlan0 down')
-down2 = os.system(f'sudo ip link set dev eth0 down')
-mac_ran_w = os.system(f'macchanger -r wlan0')
-mac_ran_e = os.system(f'macchanger -r eth0')
+ser_in = input("What you want interface change ? = ")
+down = os.system(f'sudo ip link set dev {ser_in} down')
+mac_ran_w = os.system(f'macchanger -r {ser_in}')
+up = os.system(f'sudo ip link set dev {ser_in} up')
